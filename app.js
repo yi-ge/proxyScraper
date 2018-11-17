069da199ce947ef9b63ecb63a3742467b95e40b8
Rick Terry <fdterr@gmail.com>
2018-11-16 22:04:34 -0500

    cleanup

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const ProxyVerifier = require('proxy-verifier');
-const axios = require('axios');
 const puppeteer = require('puppeteer');
-
-const output = [];
 
 const scrape = async () => {
   const browser = await puppeteer.launch({
@@ -14,24 +11,10 @@ const scrape = async () => {
   const proxies = await page.content();
   const regex = /[0-9]+(?:\.[0-9]+){3}:[0-9]+/g;
 
-  // const elite = [];
   let matches = proxies.match(regex);
-  // let result;
-  // checkIP(matches[0])
-  //   .then(outcome => {
-  //     console.log('first result is', outcome);
-  //     result = outcome;
-  //   })
-  //   .catch(err => {
-  //     console.log('ERROR!', err);
-  //     result = err;
-  //   });
   let result;
   try {
     result = await checkIP(matches[0]);
-    // matches.forEach(match => {
-    //   checkIP(match);
-    // });
     console.log('output is', result);
   } catch (err) {
     console.log(err);
@@ -45,25 +28,16 @@ const checkIP = async ipAddress => {
       ipAddress: split[0],
       port: split[1],
       protocols: ['http', 'https', 'socks4', 'socks5'],
-      // protocol: 'http',
     };
 
-    // console.log('proxy object is', proxy);
     ProxyVerifier.testAll(proxy, (error, result) => {
       if (error) {
-        console.log('error', error, 'result', result);
         resolve(error);
-        // resolve(error);
       } else {
-        console.log('error', error, 'result', result);
         resolve(result);
       }
     });
   });
-};
-
-const callback = result => {
-  return result;
 };
 
 scrape();
