81dab0fb04bb552da72692feb6dae1f2122bb436
Rick Terry <fdterr@gmail.com>
2018-11-21 01:48:50 -0500

    spys working

--- app.js
+++ app.js
@@ -6,23 +6,45 @@ let good = [];
 let bad = [];
 
 const scrape = async () => {
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
+  const browser = await puppeteer.launch(
+    // {args: ['--no-sandbox', '--disable-setuid-sandbox', '--ignoreHTTPSErrors'],}
+    { ignoreHTTPSErrors: true }
+  );
   let page = await browser.newPage();
+  page.on('console', consoleObj => {
+    console.log(consoleObj.text());
+  });
 
-  await page.goto('https://www.my-proxy.com/free-elite-proxy.html');
-  const proxies = await page.content();
-  const regex = /[0-9]+(?:\.[0-9]+){3}:[0-9]+/g;
+  // await page.goto('https://www.my-proxy.com/free-elite-proxy.html');
+  // await page.goto('https://proxy.l337.tech/txt');
+  await page.goto('http://spys.one/free-proxy-list/US/');
+  // await page.$$eval('.spy1x', elements => {
+  //   [...elements].forEach(element => {
+  //     const ip = element.querySelector()
+  //   })
+  // })
+  let ipAddresses = await page.$eval('body', element => {
+    const regex = /[0-9]+(?:\.[0-9]+){3}:[0-9]+/g;
+    let iT = element.innerText;
+    let match = iT.match(regex);
+    console.log('spy1x innertext', match.toString());
+    return match;
+  });
+  console.log('did we get a return?', ipAddresses);
 
-  let matches = proxies.match(regex);
-  let result;
+  // const proxies = await page.content();
+  // const regex = /[0-9]+(?:\.[0-9]+){3}:[0-9]+/g;
+
+  // let matches = proxies.match(regex);
+  // let result;
   // result = await checkIP(matches[0]);
   // console.log('output is', result);
-  for (let i = 0; i < 50; i++) {
-    checkIP(matches[i]);
-  }
+  // for (let i = 0; i < 50; i++) {
+  //   checkIP(matches[i]);
+  // }
 };
+
+const spysOne = async page => {};
 
 const checkIP = ipAddress => {
   // return new Promise((resolve, reject) => {
@@ -37,18 +59,20 @@ const checkIP = ipAddress => {
     if (error) {
       // console.log('error', error);
       // resolve(error);
+      console.log('there would have been an error');
     } else {
       if (result.anonymityLevel != null) {
-        console.log('result: good');
+        console.log('result: good', result);
         // ONLY INSTANCE OF GOOD RESULT
       } else {
-        console.log(
-          'result',
-          util.inspect(result.protocols.http),
-          util.inspect(result.protocols.https),
-          util.inspect(result.protocols.socks4),
-          util.inspect(result.protocols.socks5)
-        );
+        console.log('there would have been an error');
+        // console.log(
+        //   'result',
+        //   util.inspect(result.protocols.http),
+        //   util.inspect(result.protocols.https),
+        //   util.inspect(result.protocols.socks4),
+        //   util.inspect(result.protocols.socks5)
+        // );
         // }
         // resolve(result);
       }
