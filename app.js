8fdc753fec1fd7b9391bd87aa083b1bcf06949b1
Rick Terry <fdterr@gmail.com>
2018-12-12 19:35:05 -0500

    potential multi-threading working

--- app.js
+++ app.js
@@ -1,50 +1,62 @@
 const ProxyVerifier = require('proxy-verifier');
 const puppeteer = require('puppeteer');
 const util = require('util');
+const {
+  Worker,
+  isMainThread,
+  workerData,
+  parentPort,
+} = require('worker_threads');
 
 let good = [];
 let bad = [];
+let page = '';
 
 const scrape = async () => {
   const browser = await puppeteer.launch(
     // {args: ['--no-sandbox', '--disable-setuid-sandbox', '--ignoreHTTPSErrors'],}
     { ignoreHTTPSErrors: true }
   );
-  let page = await browser.newPage();
+  page = await browser.newPage();
   page.on('console', consoleObj => {
     console.log(consoleObj.text());
   });
 
-  // await page.goto('https://www.my-proxy.com/free-elite-proxy.html');
-  // await page.goto('https://proxy.l337.tech/txt');
-  await page.goto('http://spys.one/free-proxy-list/US/');
-  // await page.$$eval('.spy1x', elements => {
-  //   [...elements].forEach(element => {
-  //     const ip = element.querySelector()
-  //   })
-  // })
-  let ipAddresses = await page.$eval('body', element => {
-    const regex = /[0-9]+(?:\.[0-9]+){3}:[0-9]+/g;
-    let iT = element.innerText;
-    let match = iT.match(regex);
-    console.log('spy1x innertext', match.toString());
-    return match;
-  });
-  console.log('did we get a return?', ipAddresses);
+  let spysOneProxies = await spysOne();
+  let goodProxies = [];
+  let chunkArrays = chunkArray(spysOneProxies, 4);
+  console.log('This is the main thread');
+  for (let i = 0; i < 4; i++) {
+    let w = new Worker('./worker.js', {
+      workerData: {
+        id: i,
+        list: chunkArrays[i],
+      },
+    });
+    w.on('message', msg => {
+      console.log('message is', msg);
+    });
+  }
 
-  // const proxies = await page.content();
-  // const regex = /[0-9]+(?:\.[0-9]+){3}:[0-9]+/g;
-
-  // let matches = proxies.match(regex);
-  // let result;
-  // result = await checkIP(matches[0]);
-  // console.log('output is', result);
-  // for (let i = 0; i < 50; i++) {
-  //   checkIP(matches[i]);
-  // }
+  browser.close();
 };
 
-const spysOne = async page => {};
+const spysOne = async () => {
+  try {
+    // holder for code for now
+    await page.goto('http://spys.one/free-proxy-list/US/');
+    let ipAddresses = await page.$eval('body', element => {
+      const regex = /[0-9]+(?:\.[0-9]+){3}:[0-9]+/g;
+      let iT = element.innerText;
+      let match = iT.match(regex);
+      return match;
+    });
+    console.log('returning', ipAddresses);
+    return ipAddresses;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 const checkIP = ipAddress => {
   // return new Promise((resolve, reject) => {
@@ -66,14 +78,6 @@ const checkIP = ipAddress => {
         // ONLY INSTANCE OF GOOD RESULT
       } else {
         console.log('there would have been an error');
-        // console.log(
-        //   'result',
-        //   util.inspect(result.protocols.http),
-        //   util.inspect(result.protocols.https),
-        //   util.inspect(result.protocols.socks4),
-        //   util.inspect(result.protocols.socks5)
-        // );
-        // }
         // resolve(result);
       }
     }
@@ -81,4 +85,41 @@ const checkIP = ipAddress => {
   // });
 };
 
-scrape();
+function chunkArray(myArray, chunk_size) {
+  var results = [];
+
+  while (myArray.length) {
+    results.push(myArray.splice(0, chunk_size));
+  }
+
+  return results;
+}
+
+if (isMainThread) {
+  scrape();
+} else {
+  console.log('worker', workerData);
+  for (let i = 0; i < workerData.list; i++) {
+    let result = checkIP(workerData.list[i]);
+    if (result) {
+      parentPort.postMessage(workerData.list[i]);
+    }
+  }
+  // parentPort.postMessage(`this message comes from worker #${workerData.id}`);
+}
+
+// ProxyVerifier.testAll(proxy, (error, result) => {
+//   if (error) {
+//     // console.log('error', error);
+//     // resolve(error);
+//     console.log('there would have been an error');
+//   } else {
+//     if (result.anonymityLevel != null) {
+//       console.log('result: good', result);
+//       // ONLY INSTANCE OF GOOD RESULT
+//     } else {
+//       console.log('there would have been an error');
+//       // resolve(result);
+//     }
+//   }
+// });
---
+++ worker.js
@@ -0,0 +1,67 @@
+const ProxyVerifier = require('proxy-verifier');
+const { parentPort, workerData } = require('worker_threads');
+
+const checkIP = ipAddress => {
+  return new Promise((resolve, reject) => {
+    let split = ipAddress.split(':');
+    let proxy = {
+      ipAddress: split[0],
+      port: split[1],
+      protocols: ['http', 'https', 'socks4', 'socks5'],
+    };
+
+    ProxyVerifier.testAll(proxy, (error, result) => {
+      if (error) {
+        // console.log('error', error);
+        // resolve(error);
+        console.log('there would have been an error');
+        resolve(false);
+      } else {
+        if (result.anonymityLevel != null) {
+          console.log('result: good', ipAddress);
+          // ONLY INSTANCE OF GOOD RESULT
+          resolve(true);
+        } else {
+          console.log('there would have been an error');
+          // resolve(result);
+          resolve(false);
+        }
+      }
+    });
+  });
+};
+
+let { list } = workerData;
+for (let i = 0; i < list.length; i++) {
+  (async () => {
+    console.log(`${workerData.id} checking ${list[i]}`);
+    let result = await checkIP(list[i]);
+    console.log(`checked ${list[i]}, result is ${result}`);
+    if (result) {
+      parentPort.postMessage(list[i]);
+    }
+  })();
+}
+
+//   return ProxyVerifier.testAll(proxy, (error, result) => {
+//     if (error) {
+//       // console.log('error', error);
+//       // resolve(error);
+//       console.log('there would have been an error', ipAddress);
+//       return false;
+//     } else if (result.anonymityLevel != null) {
+//       // console.log('result: good', result);
+//       // ONLY INSTANCE OF GOOD RESULT
+//       return true;
+//     } else {
+//       console.log('there would have been an error', ipAddress);
+//       // resolve(result);
+//       return false;
+//     }
+//   });
+//   // console.log(`result for ${ipAddress} is ${result}`);
+//   // return result;
+//   // });
+// };
+
+// console.log('confirming workerData', workerData);
